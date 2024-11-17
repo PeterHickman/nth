2c6f74058afc90dcf8a493398eb64ac1d8b780ec
Peter Hickman <peterhickman386@googlemail.com>
2024-11-17 21:00:42 +0000

    clean up the build

--- install.sh
+++ install.sh
@@ -1,24 +1,13 @@
 #!/bin/sh
 
-if ! [ "$(id -u)" = 0 ]; then
-  echo 'You must be root to do this.' 1>&2
-  exit 1
-fi
+BINARY='/usr/local/bin'
+APP=nth
 
-##
-# When installing we would install as root, however on
-# BSD based systems (such as OSX) this is not a valid
-# group / user for install so we use the numeric value
-##
-ROOT="0"
+echo "Building $APP"
+go build -ldflags="-s -w" $APP.go
 
-TARGET="/usr/local/bin/nth"
+echo "Installing $APP to $BINARY"
+install $APP $BINARY
 
-echo "Building nth"
-go build nth.go
-
-echo "Installing nth to $TARGET"
-install -g $ROOT -o $ROOT -m 0755 nth $TARGET
-
-echo "Removing the build artefact"
-rm nth
+echo "Removing the build"
+rm $APP
